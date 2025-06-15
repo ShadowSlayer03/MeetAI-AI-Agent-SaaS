bde23ebd22c393c693c8122b47c5a1caeca500ef
ShadowSlayer03 <sslayer441@gmail.com>
2025-06-15 18:41:12 +0530

    Added agents setup and responsive dialog

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 function HomeView() {
-  const trpc = useTRPC();
-  const {data} = useQuery(trpc.hello.queryOptions({text: "Arjun"}))
-  if (!data) return <div>Loading...</div>;
-  return <div>{data?.greeting}</div>;
+  return <div>Homeview</div>
 }
 
 export default HomeView;
