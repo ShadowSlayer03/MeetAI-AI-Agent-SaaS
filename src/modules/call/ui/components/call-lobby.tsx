818c9ae4c0d1c4c6d4780adcdcf50a834f6958fe
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2025-06-27 17:42:44 +0530

    Update src/modules/call/ui/components/call-lobby.tsx
    Fixed classname typo
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/call/ui/components/call-lobby.tsx
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -56,7 +56,7 @@ const CallLobby = ({ onJoin }: Props) => {
   const hasBrowserMediaPermission = hasMicPermission && hasCameraPermission;
 
   return (
-    <div className="flex flex-col items-cetner justify-center h-full bg-radial from-sidebar-accent to-sidebar">
+    <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
       <div className="py-4 px-8 flex-1 items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
           <div className="flex flex-col gap-y-2 text-center">
